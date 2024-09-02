6071c433814cec3c0beabc1b7dddf47d2b89025d
skhan123-noncoder <soumenkhan123@gmail.com>
2024-09-02 18:10:09 +0100

    Added update ticker def and rsi calcs

---
+++ update_ticker.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import datetime
+import yfinance as yf
+
+
+def update_data(file_name, ticker_symbol):
+    # Read data into a data-frame
+    df = pd.read_csv(file_name)
+
+    # Get the last entry and increase by 1 day
+    start_date = datetime.datetime.strptime(((df.iloc[-1]).Date), '%Y-%m-%d')
+    start_date += datetime.timedelta(days=1)
+    
+    # Get today's date
+    today = datetime.date.today().strftime('%Y-%m-%d')
+
+    #Check if the data is already imported
+    if start_date.strftime('%Y-%m-%d') == datetime.date.today().strftime('%Y-%m-%d'):
+        print("Stock info up to date")
+    else:
+        ticker = yf.Ticker(ticker_symbol)
+        data = ticker.history(start=start_date, end=datetime.date.today().strftime('%Y-%m-%d'))
+        data.reset_index(inplace=True)
+
+    # Check if the 'Date' column is of datetime type
+        if pd.api.types.is_datetime64_any_dtype(data['Date']):
+            data['Date'] = data['Date'].dt.strftime('%Y-%m-%d')
+
+    #Add the new rows to the existing df
+        df_new = pd.concat([df, data], ignore_index=True)
+    
+        return(df_new)
+
+if __name__=='__main__':
+    update_data()
